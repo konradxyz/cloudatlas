7896684f5c32ced0ec132b7b2260ffe76712fb2f
mello <mello@mello.(none)>
2014-10-10 19:46:20 +0200

    Added dummy tests

---
+++ src/test/Success.java
@@ -0,0 +1,15 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Success {
+
+	@Test
+	public void test() {
+		// Doing nothing.
+		assertTrue(true);
+	}
+
+}
