1946b560e7c1e3fce3308e80646360811e13f439
Konrad Paziewski <kp306410@students.mimuw.edu.pl>
2014-10-27 22:09:23 +0100

    Added more tests for binary operators

--- src/test/ExpressionInterpreterTest.java
+++ src/test/ExpressionInterpreterTest.java
@@ -5,8 +5,11 @@ import java.util.List;
 
 import pl.edu.mimuw.cloudatlas.model.Value;
 import pl.edu.mimuw.cloudatlas.model.ValueBoolean;
+import pl.edu.mimuw.cloudatlas.model.ValueDouble;
+import pl.edu.mimuw.cloudatlas.model.ValueInt;
+import pl.edu.mimuw.cloudatlas.model.ValueString;
 
-public class BinaryInterpreterTest extends InterpreterTest {
+public class ExpressionInterpreterTest extends InterpreterTest {
 	private List<TestCase> tests = new ArrayList<TestCase>();
 	
 	
@@ -26,6 +29,21 @@ public class BinaryInterpreterTest extends InterpreterTest {
 	private void t(String expression) {
 		r(expression, new ValueBoolean(true));
 	}
+	
+	private void i(String expression, long result) {
+		r(expression, new ValueInt(result));
+	}
+	
+
+	private void d(String expression, double result) {
+		r(expression, new ValueDouble(result));
+	}
+	
+
+	private void s(String expression, String result) {
+		r(expression, new ValueString(result));
+	}
+
 	
 	@Override
 	public List<TestCase> getTests() {
@@ -54,8 +72,23 @@ public class BinaryInterpreterTest extends InterpreterTest {
 		f("1.0 >= 2.0");
 		t("\"m\" >= \"a\"");
 		
+		i("1 + 1", 2);
+		i("1 - 1", 0);
+		i("3 * 4", 12);
+		i("4 % 3", 1);
 		
 		
+		// Division returns double
+		d("4 / 3", 4. / 3.);
+		
+		// We allow to add strings
+		s("\"ab\" + \" \" + \"cd\"", "ab cd");
+		
+		t("true AND true");
+		f("true AND false");
+		
+		t("true OR false");
+		f("false OR false");
 		
 		
 		return tests;
