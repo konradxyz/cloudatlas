cd461898f198dddb010b43fddc4ade44aa10d4cd
Konrad Paziewski <kp306410@students.mimuw.edu.pl>
2014-12-10 20:03:45 +0100

    Rmi

---
+++ src/pl/edu/mimuw/cloudatlas/common/rmi/CloudatlasAgentRmiServer.java
@@ -0,0 +1,10 @@
+package pl.edu.mimuw.cloudatlas.common.rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import pl.edu.mimuw.cloudatlas.common.model.Value;
+
+public interface CloudatlasAgentRmiServer extends Remote {
+	public void setCurrentNodeAttribute(String attribute, Value value) throws RemoteException;
+}
