c5a0690007de6b0477658e3c2b8616155e57b5a7
Konrad Paziewski <kp306410@students.mimuw.edu.pl>
2014-12-12 05:39:57 +0100

    Created node client

--- src/pl/edu/mimuw/cloudatlas/nodeclient/Main.java
+++ src/pl/edu/mimuw/cloudatlas/nodeclient/Main.java
@@ -1,34 +1,13 @@
 package pl.edu.mimuw.cloudatlas.nodeclient;
 
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-import java.util.Random;
-
-import pl.edu.mimuw.cloudatlas.common.model.ValueInt;
 import pl.edu.mimuw.cloudatlas.common.rmi.CloudatlasAgentRmiServer;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Random r = new Random();
-		while (true) {
-			try {
-				Registry registry = LocateRegistry.getRegistry("localhost",
-						CloudatlasAgentRmiServer.DEFAULT_PORT);
-				CloudatlasAgentRmiServer stub = (CloudatlasAgentRmiServer) registry
-						.lookup("cloudatlas");
-				stub.setCurrentNodeAttribute("random",
-						new ValueInt(r.nextLong()));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-
+		NodeClient nodeClient = new NodeClient("localhost",
+				CloudatlasAgentRmiServer.DEFAULT_PORT, 2000);
+		nodeClient.run();
 	}
 
 }
