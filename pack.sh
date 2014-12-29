ae473557d021ab7ec24e3a92cdfb1d4deedc7e11
Konrad Paziewski <kp306410@students.mimuw.edu.pl>
2014-12-29 01:02:11 +0100

    Task 2

--- pack.sh
+++ pack.sh
@@ -2,6 +2,6 @@
 
 set -eax
 
-DATE=`date +"%Y%M%d%H%M%S"`
+DATE=`date +"%Y%m%d%H%M%S"`
 
-tar -cvvf cloudatlas_kz306486_kp306410_$DATE.tar.gz src build.xml *.policy agent.sh nodeclient.sh webclient.sh querysigner.sh README lib config web templates 
+tar -jcvf cloudatlas_kz306486_kp306410_$DATE.tar.gz src build.xml *.policy agent.sh nodeclient.sh webclient.sh signer.sh README config web templates 
